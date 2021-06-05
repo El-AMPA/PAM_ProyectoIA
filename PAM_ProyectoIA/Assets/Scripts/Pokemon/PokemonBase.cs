1b383f1e413c4af1d8465ab2aa28c29ef2d5c038
Miguel Mur Cortés <mmur@ucm.es>
2021-06-05 19:23:32 +0200

    Tabla de tipos metida. Aun no furula
    Co-Authored-By: Pj <47722275+pj6595@users.noreply.github.com>
    Co-Authored-By: Auraum <47794404+auraum@users.noreply.github.com>

--- PAM_ProyectoIA/Assets/Scripts/Pokemon/PokemonBase.cs
+++ PAM_ProyectoIA/Assets/Scripts/Pokemon/PokemonBase.cs
@@ -111,3 +111,27 @@ public enum PokemonType
     Ghost,
     Dragon
 }
+
+public class TypeChart
+{
+    float[][] chart =
+   {//            NOR FIR WAT ELE GRA ICE FIG POI GRO FLY PSY BUG ROC GHO DRA DAR STE
+     /*NOR*/ new float[]{ 1f,1f,1f,1f,1f,1f,1f,1f,1f,1f,1f,1f,0.5f,0f, 1f, 1f, 0.5f},
+     /*FIR*/ new float[]{ 1f,0.5f,0.5f,1f,2f,2f,1f,1f,1f,1f,1f,2f,0.5f,1f, 0.5f, 1f, 2f},
+     /*WAT*/ new float[]{ 1f,2f,0.5f,1f,0.5f,1f,1f,1f,2f,1f,1f,1f,2f,1f, 0.5f, 1f, 1f},
+     /*ELE*/ new float[]{ 1f,2f,0.5f,0.5f,1f,1f,1f,0f,2f,1f,1f,1f,1f,1f, 0.5f, 1f, 1f},
+     /*GRA*/ new float[]{ 1f,0.5f,2f,1f,0.5f,1f,1f,0.5f,2f,0.5f,1f,0.5f,2f,1f, 0.5f, 1f, 0.5f},
+     /*ICE*/ new float[]{ 1f,0.5f,0.5f,1f,2f,0.5f,1f,1f,2f,2f,1f,1f,1f,1f, 2f, 1f, 0.5f},
+     /*FIG*/ new float[]{ 2f,1f,1f,1f,1f,2f,1f,0.5f,1f,0.5f,0.5f,0.5f,2f,0f, 1f, 2f, 2f},
+     /*POI*/ new float[]{ 1f,1f,1f,1f,2f,1f,1f,0.5f,0.5f,1f,1f,1f,0.5f,0.5f, 1f, 1f, 0f},
+     /*GRO*/ new float[]{ 1f,2f,1f,2f,0.5f,1f,1f,2f,0f,1f,0.5f,2f,1f,1f, 1f, 1f, 2f},
+     /*FLY*/ new float[]{ 1f,1f,1f,0.5f,2f,1f,2f,1f,1f,1f,1f,2f,0.5f,1f, 1f, 1f, 0.5f},
+     /*PSY*/ new float[]{ 1f,1f,1f,1f,1f,1f,2f,2f,1f,1f,0.5f,1f,1f,1f, 1f, 0f, 0.5f},
+     /*BUG*/ new float[]{ 1f,0.5f,1f,1f,2f,1f,0.5f,0.5f,1f,0.5f,2f,1f,1f,0.5f, 1f, 2f, 0.5f},
+     /*ROC*/ new float[]{ 1f,2f,1f,1f,1f,2f,0.5f,1f,0.5f,2f,1f,2f,1f,1f, 1f, 1f, 0.5f},
+     /*GHO*/ new float[]{ 0f,1f,1f,1f,1f,1f,1f,1f,1f,1f,2f,1f,1f,2f, 1f, 0.5f, 1f},
+     /*DRA*/ new float[]{ 1f,1f,1f,1f,1f,1f,1f,1f,1f,1f,2f,1f,1f,1f, 2f, 1f, 0.5f},
+     /*DAR*/ new float[]{ 1f,1f,1f,1f,1f,1f,0.5f,1f,1f,1f,2f,1f,1f,2f, 1f, 0.5f, 1f},
+     /*STE*/ new float[]{ 1f,0.5f,0.5f,0.5f,1f,2f,1f,1f,1f,1f,1f,1f,2f,1f, 1f, 1f, 0.5f},
+    };
+}
