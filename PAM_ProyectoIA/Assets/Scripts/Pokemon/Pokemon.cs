c487c6427c7912b038119ae45db6addb0c9a2782
Miguel Mur Cortés <mmur@ucm.es>
2021-06-05 14:27:01 +0200

    Interfaz básica de combate. Pilla los datos de los pokemon pero aun no hace nada más
    Co-Authored-By: Pj <47722275+pj6595@users.noreply.github.com>
    Co-Authored-By: Auraum <47794404+auraum@users.noreply.github.com>

---
+++ PAM_ProyectoIA/Assets/Scripts/Pokemon/Pokemon.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    public PokemonBase Base { get; set; }
+    public int Level { get; set; }
+
+    public int HP { get; set; }
+    public List<Move> Moves { get; set; }
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        Base = pBase;
+        Level = pLevel;
+        HP = MaxHP;
+    }
+
+    public int MaxHP
+    {
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 10; }
+    }
+
+    public int Attack
+    {
+        get { return Mathf.FloorToInt((Base.Attack * Level) / 100f) + 5; }
+    }
+
+    public int Deffense
+    {
+        get { return Mathf.FloorToInt((Base.Deffense * Level) / 100f) + 5; }
+    }
+
+    public int SpAttack
+    {
+        get { return Mathf.FloorToInt((Base.SpAttack * Level) / 100f) + 5; }
+    }
+
+    public int SpDefense
+    {
+        get { return Mathf.FloorToInt((Base.SpDefense * Level) / 100f) + 5; }
+    }
+
+    public int Speed
+    {
+        get { return Mathf.FloorToInt((Base.Speed * Level) / 100f) + 5; }
+    }
+}
