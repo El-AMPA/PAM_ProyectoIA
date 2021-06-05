1a7ae184e3470420723029b9449723ca37b86821
Miguel Mur Cortés <mmur@ucm.es>
2021-06-05 17:01:16 +0200

    Selector de acciones y se muestra el menu de movimientos
    Co-Authored-By: Pj <47722275+pj6595@users.noreply.github.com>
    Co-Authored-By: Auraum <47794404+auraum@users.noreply.github.com>

--- PAM_ProyectoIA/Assets/Scripts/Battle/BattleDialogBox.cs
+++ PAM_ProyectoIA/Assets/Scripts/Battle/BattleDialogBox.cs
@@ -8,6 +8,18 @@ public class BattleDialogBox : MonoBehaviour
     [SerializeField] Text dialogText;
     [SerializeField] int lettersPerSecond;
 
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject moveSelector;
+    [SerializeField] GameObject moveDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> moveTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
+    [SerializeField] Color highlightedColor;
+
     public void SetDialog(string dialog)
     {
         dialogText.text = dialog;
@@ -20,6 +32,34 @@ public class BattleDialogBox : MonoBehaviour
         {
             dialogText.text += letter;
             yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+    }
+
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (int i = 0; i < actionTexts.Count; i++)
+        {
+            if (i == selectedAction)
+            {
+                actionTexts[i].color = highlightedColor;
+            }
+            else actionTexts[i].color = Color.black;
         }
     }
 }
--- PAM_ProyectoIA/Assets/Scripts/Battle/BattleSystem.cs
+++ PAM_ProyectoIA/Assets/Scripts/Battle/BattleSystem.cs
@@ -2,26 +2,86 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy }
+
 public class BattleSystem : MonoBehaviour
 {
+
     [SerializeField] BattleUnit playerUnit;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleHud enemyHud;
     [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;
+    int currentActon;
+
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    public IEnumerator SetupBattle()
     {
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
 
-        StartCoroutine(dialogBox.TypeDialog($"A wild {playerUnit.Pokemon.Base.Name} appeared."));
+        yield return dialogBox.TypeDialog($"A wild {playerUnit.Pokemon.Base.Name} appeared.");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentActon < 1) currentActon++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentActon > 0) currentActon--;
+        }
+
+        dialogBox.UpdateActionSelection(currentActon);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if(currentActon == 0)
+            {
+                //Fight
+                PlayerMove();
+            }
+            
+            else if (currentActon == 1)
+            {
+                //Run
+            }
+        }
     }
 }
